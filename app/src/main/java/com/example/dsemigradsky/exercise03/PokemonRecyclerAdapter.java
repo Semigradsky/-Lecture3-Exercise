83280d4c1a0c305b4ae5ef1beced975fefebaf57
Dmitry Semigradsky <semigradskyd@gmail.com>
2018-08-03 12:29:18 +0300

    Add solution

---
+++ app/src/main/java/com/example/dsemigradsky/exercise03/PokemonRecyclerAdapter.java
@@ -0,0 +1,64 @@
+package com.example.dsemigradsky.exercise03;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ahmadrosid.svgloader.SvgLoader;
+
+import java.util.List;
+
+public class PokemonRecyclerAdapter extends RecyclerView.Adapter<PokemonRecyclerAdapter.ViewHolder> {
+
+    private final List<Pokemon> pokemons;
+    private final Context context;
+    private final LayoutInflater inflater;
+    private View.OnClickListener onClickListener;
+
+    PokemonRecyclerAdapter(Context context, List<Pokemon> pokemons, View.OnClickListener onClickListener) {
+        this.context = context;
+        this.pokemons = pokemons;
+        this.inflater = LayoutInflater.from(context);
+        this.onClickListener = onClickListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewHolder, int i) {
+        View view = inflater.inflate(R.layout.list_item_pokemon, viewHolder, false);
+        view.setOnClickListener(onClickListener);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Pokemon pokemon = pokemons.get(i);
+
+        viewHolder.name.setText(pokemon.getName());
+        SvgLoader.pluck().with((Activity)context).load(pokemon.getAvatar(), viewHolder.avatar);
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.pokemons.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        public final ImageView avatar;
+        public final TextView name;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            this.avatar = itemView.findViewById(R.id.avatar);
+            this.name = itemView.findViewById(R.id.name);
+        }
+    }
+}
